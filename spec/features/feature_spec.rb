11de47c21993310505abbf0388d12c1a28163456
Dan Groze <dangroze@Dans-MBP.home>
2019-01-26 22:07:00 +0000

    Feature test- registering name

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,9 +1,12 @@
-feature 'Testing infrastructure' do
-  scenario 'can run app and check page content' do
-    visit('/')
-    expect(page).to have_content 'Hello hello'
-  end
-end
+require 'spec_helper'
+
 # As a marketeer
 # So that I can see my name in lights
 # I would like to register my name before playing an online game
+
+feature 'Registering name' do
+  scenario 'can see name' do
+    sign_in_and_play
+    expect(page).to have_content('Dan')
+  end
+end

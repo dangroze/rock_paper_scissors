54175a21be59410903591dfae12bb70c615c856b
Dan Groze <dangroze@Dans-MBP.home>
2019-01-26 22:43:31 +0000

    Added name feature

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
+
   get '/' do
-    'Hello hello!'
+    erb :index
+  end
+
+  post '/name' do
+    session[:player1_name] = params[:player1_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1_name = session[:player1_name]
+    erb :play
   end
 
   run! if app_file == $0

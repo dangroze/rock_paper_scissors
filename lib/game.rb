9d71c35dc474b90ab555fe292154d328e3958d5a
Dan Groze <dangroze@Dans-MBP.home>
2019-01-27 15:24:00 +0000

    Initialized Game with player and weapon

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,8 @@
 class Game
+  attr_reader :player, :weapon
+
+  def initialize(player, weapon)
+    @player = player
+    @weapon = weapon
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,20 @@
 require 'game'
 
 RSpec.describe Game do
-  
+  subject(:game) { described_class.new(player, weapon) }
+  let(:player) { double :player }
+  let(:weapon) { double :weapon }
+
+  describe "#player" do
+    it "registers a player" do
+      expect(game.player).to eq player
+    end
+  end
+
+  describe '#weapon' do
+    it 'selects a weapon' do
+      expect(game.weapon).to eq weapon
+    end
+  end
+
 end

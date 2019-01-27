503b4e0556b2860941a78fe2719f2ab5e55e5f00
Dan Groze <dangroze@Dans-MBP.home>
2019-01-27 19:02:04 +0000

    Added game #rule test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,19 +2,24 @@ require 'game'
 
 RSpec.describe Game do
   subject(:game) { described_class.new(player, weapon) }
-  let(:player) { double :player }
-  let(:weapon) { double :weapon }
-
+  let(:player) { Player.new('Dan') }
+  let(:weapon) { 'Rock' }
   describe "#player" do
     it "registers a player" do
-      expect(game.player).to eq player
+      expect(game.player.name).to eq 'Dan'
     end
   end
 
   describe '#weapon' do
     it 'selects a weapon' do
-      expect(game.weapon).to eq weapon
+      expect(game.weapon).to eq 'Rock'
     end
   end
 
+  describe '#rule' do
+    it 'describes the game rules' do
+      allow(game).to receive(:rule).and_return :win
+      expect(game.rule).to eq :win
+    end
+  end
 end

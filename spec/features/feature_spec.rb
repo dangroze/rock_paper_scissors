706ca4708efbe0ee7d18846bd952fd19ed19a033
Dan Groze <dangroze@Dans-MBP.home>
2019-01-27 10:01:35 +0000

    added tests for seeing Rock, Paper, Scissors on page

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -4,9 +4,21 @@ require 'spec_helper'
 # So that I can see my name in lights
 # I would like to register my name before playing an online game
 
-feature 'Registering name' do
-  scenario 'can see name' do
+feature 'Playing a game' do
+  before do
     sign_in_and_play
+  end
+
+  scenario 'can see name' do
     expect(page).to have_content('Dan')
+end
+
+# As a marketeer
+# I want to see the choices(rock, paper, scissors)
+
+  scenario 'can see Rock, Paper, Scissors' do
+    expect(page).to have_content('Rock')
+    expect(page).to have_content('Paper')
+    expect(page).to have_content('Scissors')
   end
 end
